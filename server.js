8045d414cc1d3160b1da038f4ad47b5cce94636b
Matt <mparman6@yahoo.com>
2016-02-06 18:00:51 -0500

    basic planning session

---
+++ server.js
@@ -0,0 +1,7 @@
+// Edit portfolio page so that it contains RPS game and Clicky game
+
+// Create route handling in server for all pages and 404 page
+
+// Create a login page with session that eventually expires
+
+// Redeploy to heroku
