9f61454702f72177cfd71e2ce683df2133ebcafd
Matt <mparman6@yahoo.com>
2016-02-08 17:54:32 -0500

    file paths work from diffferent pages

--- server.js
+++ server.js
@@ -5,3 +5,36 @@
 // Create a login page with session that eventually expires
 
 // Redeploy to heroku
+
+var express = require('express');
+
+
+var app = express();
+var PORT = process.env.PORT || 8090;
+
+app.use("/js", express.static("public/js"));
+app.use("/css", express.static("public/css"));
+
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.get("/about", function(req, res) {
+  res.sendFile(__dirname + '/views/about.html');
+});
+
+app.get("/portfolio", function(req, res) {
+  res.sendFile(process.cwd() + "/views/portfolio.html");
+});
+
+app.get("/services", function(req, res) {
+  res.sendFile(process.cwd() + "/views/services.html");
+});
+
+app.get("/contact", function(req, res) {
+  res.sendFile(process.cwd() + "/views/contact.html");
+});
+
+app.listen(PORT, function() {
+  console.log("Listening on port %s", PORT);
+});
